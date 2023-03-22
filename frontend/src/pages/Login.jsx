af0182c3d39a5e3faf8e93ea3ee4f91f378fea2f
kiprotichdominic <kiprotichdominic102@gmail.com>
2023-03-23 01:09:56 +0300

    add login page UI

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import { useNavigate } from "react-router-dom";
 
 function Login() {
+  const navigate = useNavigate()
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log("Submitted........");
+    navigate("/dashboard")
+  };
   return (
-    <div>Login</div>
-  )
+    <div className="flex justify-center items-center h-screen">
+      <div className="w-96 h-96 flex justify-center items-center">
+        <form
+          className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-96 h-auto"
+          onSubmit={handleLogin}>
+          <h2 className="text-2xl font-bold mb-4 text-center">Basigo Login</h2>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 font-bold mb-2"
+              htmlFor="username">
+              Username
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="username"
+              type="text"
+              placeholder="Enter your username"
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 font-bold mb-2"
+              htmlFor="password">
+              Password
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="Enter your password"
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit">
+              Log In
+            </button>
+            <a
+              className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+              href="#">
+              Forgot Password?
+            </a>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
